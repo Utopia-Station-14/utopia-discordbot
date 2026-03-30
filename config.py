0fee12876fdd5dd487d1750e41043c10fd5545f0
M1and1B <153716654+M1and1B@users.noreply.github.com>
2026-03-30 19:52:36 +0300

    bot-add

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_KEY = os.getenv("DISCORD_KEY")
---
+++ main.py
@@ -0,0 +1,21 @@
+from init import bot
+import importlib
+import pkgutil
+from pathlib import Path
+from config import DISCORD_KEY
+
+
+def load_modules(folder: str):
+    package = importlib.import_module(folder)
+    if not package.__file__:
+        return
+
+    dir_path = Path(package.__file__).parent
+
+    for _, mod_name, _ in pkgutil.iter_modules([str(dir_path)]):
+        importlib.import_module(f"{folder}.{mod_name}")
+
+
+load_modules('commands')
+
+bot.run(DISCORD_KEY)
