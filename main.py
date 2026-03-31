f638742159015ac16f40553c0ee74ec7a56540b4
M1and1B <153716654+M1and1B@users.noreply.github.com>
2026-03-31 20:01:25 +0300

    123

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from init import bot
 import importlib
 import pkgutil
-from config import DISCORD_KEY
+import os
 from discord.ext import commands as discord_commands
+
 
 def load_modules(folder: str):
     package = importlib.import_module(folder)
@@ -14,17 +15,16 @@ def load_modules(folder: str):
 load_modules("commands")
 load_modules("helpers")
 
+
 @bot.event
 async def on_ready():
     print(f"Бот запущен как {bot.user}")
 
-    channel = bot.get_channel(1487927594870374531)
+    channel = await bot.fetch_channel(1487927594870374531)
 
-    if channel:
-        await channel.send("Проверка модулей... ожидайте.")
-        await channel.send("Здравствуйте, я проснулась!")
-    else:
-        print("Не удалось найти канал для логов")
+    await channel.send("Проверка модулей... ожидайте.")
+    await channel.send("Здравствуйте, я проснулась!")
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -33,11 +33,12 @@ async def on_command_error(ctx, error):
         return await ctx.send("Я нихуя не поняла.")
 
     if isinstance(error, discord_commands.MissingPermissions):
-        return await ctx.send("ТЫ НЕ ПРОЙДЕЕЕЕШЬ.")
+        return await ctx.send("ТЫ НЕ ПРОЙДЕЕЕШЬ.")
 
     if isinstance(error, discord_commands.MissingAnyRole):
         return await ctx.send("Приятель, а не пошёл бы ты нахуй.")
 
     await ctx.send(f"⚠️ Ошибка: {error}")
 
-bot.run(DISCORD_KEY)
+
+bot.run(os.getenv("DISCORD_KEY"))
